db9d7b3375878502f9394cb1cd8fd3a8c6366dbe
ViktorijaBe <ViktorijaBe@users.noreply.github.com>
2015-03-19 12:47:41 +0100

    Update and rename bmbf.php to bbsr.php

--- bmbf.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-$filestream = fopen("http://www.bmbf.de/foerderungen/677.php", "r"); 
-while(!feof($filestream)) { 
-    $buffer = fgets($filestream, 4096); 
-    echo $buffer; 
-} 
-fclose($filestream); 
-?>
